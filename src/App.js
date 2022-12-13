08363d06540e92c2ffd647ebf8425f9f733ca185
Oyimhon <97nizamova97@gmail.com>
2022-12-13 23:11:37 +0500

    init:test application completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
+import React, { useEffect, useMemo, useState } from 'react';
+
+import { Data } from './data';
+import { shuffle } from './utils';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [answered, setAnswered] = useState([]);
+  const [finish, setFinish] = useState(false);
+  const [anim, setAnim] = useState(false);
+  const [notFinished, setNotFinished] = useState(false);
+
+  useEffect(() => {
+    const shuffled = Data.map(item =>
+      Object.assign({}, { ...item, answers: shuffle(item.answers) })
+    );
+    setList(shuffle(shuffled));
+
+    return () => {
+      setList([]);
+      setAnswered([]);
+      setFinish(false);
+      setNotFinished(false);
+    };
+  }, []);
+
+  const handleAnswers = (id, elem) => {
+    const filtered = list.map(item =>
+      item.id === id
+        ? { ...item, correct: elem.correct ? `${id}` : 'not' }
+        : item
+    );
+    setList(filtered);
+
+    setAnswered([...answered, { correct: elem.correct ? `${id}` : '' }]);
+  };
+
+  const handleSubmit = () => {
+    if (answered.length === list.length) {
+      setFinish(true);
+      setNotFinished(false);
+    } else {
+      setNotFinished(true);
+    }
+  };
+
+  const lengthCorrectAnswer = useMemo(
+    () => answered.filter(item => item.correct).length,
+    [answered]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__inner">
+        <div className="App__wrap">
+          <h2>Определение знаний студентов по дисцеплине "Философия"</h2>
+          <h3>
+            По окончании теста вы увидите правильные ответы. Прочитайте
+            внимательно вопрос и выберите один из предложенных вариантов
+            ответов.
+          </h3>
+        </div>
+        {list.map((item, index) => (
+          <div
+            key={item.id}
+            className={
+              item.correct === String(item.id)
+                ? 'App__wrap green'
+                : item.correct === 'not'
+                ? 'App__wrap red'
+                : 'App__wrap'
+            }
+          >
+            <div className="App__top">
+              <p className="App__quest">{item.name}</p>
+            </div>
+            <div className="App__bottom">
+              {item.answers.map((elem, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleAnswers(item.id, elem)}
+                  className={
+                    item.correct === String(item.id) && elem.correct
+                      ? 'checkedAnswer green'
+                      : item.correct === 'not' && elem.correct === false
+                      ? 'checkedAnswer '
+                      : 'checkedAnswer'
+                  }
+                >
+                  {elem.answer}
+                </button>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div className="App__wrap submit">
+          <button className="button" onClick={handleSubmit}>
+            Отправить
+          </button>
+          {finish && (
+            <div
+              className={`loader ${finish && 'active'}`}
+              onAnimationEnd={() => setAnim(true)}
+            >
+              <div className={`check ${anim && 'active'}`}>
+                <span className="check-one"></span>
+                <span className="check-two"></span>
+              </div>
+            </div>
+          )}
+        </div>
+        {notFinished && (
+          <div className="notFinished">Вы не ответили на все вопросы!</div>
+        )}
+        {finish && answered.length === list.length && (
+          <div className="App__wrap answered">
+            {lengthCorrectAnswer >= 8
+              ? 'Отлично'
+              : lengthCorrectAnswer > 5
+              ? 'Хорошо'
+              : 'Очень плохо'}
+            {': '}
+            {lengthCorrectAnswer + ' из ' + answered.length}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,266 @@
+export const Data = [
+  {
+    id: 1,
+    name: 'Как с греческого переводится монизм?',
+    answers: [
+      {
+        answer: 'единичный',
+        correct: true
+      },
+      {
+        answer: 'двойной',
+        correct: false
+      },
+      {
+        answer: 'тройной',
+        correct: false
+      },
+      {
+        answer: 'бесконечный',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Как переводится сциентизм (stientya)?',
+    answers: [
+      {
+        answer: 'знание',
+        correct: true
+      },
+      {
+        answer: 'мысль',
+        correct: false
+      },
+      {
+        answer: 'учение',
+        correct: false
+      },
+      {
+        answer: 'опыт',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Каким философским учениям является Санкхья?',
+    answers: [
+      {
+        answer: 'отвлечение духа от материи',
+        correct: true
+      },
+      {
+        answer: 'плюралистическое',
+        correct: false
+      },
+      {
+        answer: 'дуалистическим',
+        correct: false
+      },
+      {
+        answer: 'единое начало как душа',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Какое учение основывается на идеи о том, что  основу мира оставляет  субстанция , одна причина?',
+    answers: [
+      {
+        answer: 'монизм',
+        correct: true
+      },
+      {
+        answer: 'дуализм',
+        correct: false
+      },
+      {
+        answer: 'плюрализм',
+        correct: false
+      },
+      {
+        answer: 'иделиализм',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Главный принцип античного философского мировоззрения это…',
+    answers: [
+      {
+        answer: 'космоцентризм',
+        correct: true
+      },
+      {
+        answer: 'антропоцентризм',
+        correct: false
+      },
+      {
+        answer: 'теоцентризм',
+        correct: false
+      },
+      {
+        answer: 'пантеизм',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: ' В философии Платона идеи – это',
+    answers: [
+      {
+        answer: 'источник всего',
+        correct: true
+      },
+      {
+        answer: 'мысли',
+        correct: false
+      },
+      {
+        answer: 'одна из основных форм человеческого познания',
+        correct: false
+      },
+      {
+        answer: 'понятие разума, которому ничего не соответствует в опыте',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Переходный период от средневековой культуры к новоевропейской культуре',
+    answers: [
+      {
+        answer: 'эпоха возрождения',
+        correct: true
+      },
+      {
+        answer: 'средневековье',
+        correct: false
+      },
+      {
+        answer: 'античность',
+        correct: false
+      },
+      {
+        answer: 'нового времени',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 8,
+    name: 'Анторопотеизм  это',
+    answers: [
+      {
+        answer: 'богочеловек',
+        correct: true
+      },
+      {
+        answer: 'сверхчеловека',
+        correct: false
+      },
+      {
+        answer: 'всесущее',
+        correct: false
+      },
+      {
+        answer: 'не человека',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 9,
+    name: ' В эпоху Нового времени философия ориентируется на ?',
+    answers: [
+      {
+        answer: 'на естественные, точные науки',
+        correct: true
+      },
+      {
+        answer: 'на гуманитарные науки',
+        correct: false
+      },
+      {
+        answer: 'на религию',
+        correct: false
+      },
+      {
+        answer: 'на политику',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 10,
+    name: ' Как с древнегреческого языка переводится философия?',
+    answers: [
+      {
+        answer: ' любовь -  мудрость ',
+        correct: true
+      },
+      {
+        answer: 'ненавидеть- мудрость',
+        correct: false
+      },
+      {
+        answer: 'любовь – жизнь',
+        correct: false
+      },
+      {
+        answer: 'познание – учение',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 11,
+    name: 'Какое учение утверждает что в основе бытия лежит множество разнообразных субстанций ?',
+    answers: [
+      {
+        answer: 'плюрализм ',
+        correct: true
+      },
+      {
+        answer: 'монизм',
+        correct: false
+      },
+      {
+        answer: 'идеализм',
+        correct: false
+      },
+      {
+        answer: 'дуализм',
+        correct: false
+      }
+    ]
+  },
+  {
+    id: 12,
+    name: 'Как с латинского переводится слово плюрал?',
+    answers: [
+      {
+        answer: 'множество',
+        correct: true
+      },
+      {
+        answer: 'бесконечно',
+        correct: false
+      },
+      {
+        answer: 'один',
+        correct: false
+      },
+      {
+        answer: 'безразмерный',
+        correct: false
+      }
+    ]
+  }
+];
